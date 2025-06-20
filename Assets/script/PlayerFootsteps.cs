95cdabdc3867f045aa8fad51aec8c2840743a4c5
JordiFRB <jordirivasbf@gmail.com>
2025-06-19 23:41:47 -0600

    Update PlayerFootsteps.cs

--- Assets/script/PlayerFootsteps.cs
+++ Assets/script/PlayerFootsteps.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class PlayerFootsteps
+[RequireComponent(typeof(CharacterController))]
+public class PlayerFootsteps : MonoBehaviour
 {
-    
+    public AudioSource footstepAudio; // referencia al audio
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded && controller.velocity.magnitude > 0.2f)
+        {
+            if (!footstepAudio.isPlaying)
+                footstepAudio.Play();
+        }
+        else
+        {
+            if (footstepAudio.isPlaying)
+                footstepAudio.Stop();
+        }
+    }
 }
